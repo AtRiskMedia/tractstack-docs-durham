670bb62b0a8b35a628ce9891766e14953aea57d4
Josh Frederick <1joshfrederick@gmail.com>
2024-04-16 14:34:52 -0400

    Added Tailwind css to sponsors.astro
    -Imported Tailwind.css to the project
    -Added space between the sponsors and feedback
    -Set the feedback content_id as an environment variable (Not working right now)
    -Temporarily set a twitter link for discord social to prevent a bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,65 +1,63 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: 'TractStack Docs',
-      pagefind: false,
-      favicon: 'favicon.ico',
-	  components:{
-		TableOfContents: '/src/components/TableOfContents.astro'
-	  },
-      logo: {
-        src: '/src/assets/tractstack-banner-logo.png',
-        replacesTitle: true,
+  integrations: [starlight({
+    title: 'TractStack Docs',
+    pagefind: false,
+    favicon: 'favicon.ico',
+    components: {
+      TableOfContents: '/src/components/TableOfContents.astro'
+    },
+    logo: {
+      src: '/src/assets/tractstack-banner-logo.png',
+      replacesTitle: true
+    },
+    defaultLocale: 'en',
+    locales: {
+      en: {
+        label: 'English'
       },
-      defaultLocale: 'en',
-      locales: {
-        en: {
-          label: 'English',
-        },
-        fr: {
-          label: 'Français',
-          lang: 'fr',
-        },
-      },
-	  social: {
-		github: 'https://github.com/AtRiskMedia',
-		twitter: 'https://twitter.com/AtRiskMedia',
-		discord:'',
-	  },
-      customCss: [
-        '@fontsource/outfit',
-        './src/styles/custom.css',
-      ],
-      sidebar: [
-        {
-          label: 'Start here!',
-          items: [
-            { label: 'Getting Started', link: '/start-here/getting-started', badge: { text: 'Important!', variant: 'success'} },
-			// { label: '', link: '/' },
-          ]
-        },
-        {
-          label: 'Core Concepts',
-          items: [
-			// { label: '', link: '/' },
-          ]
-        },
-        {
-          label: 'Tutorials',
-          items: [
-            // { label: '', link: '/' },
-          ]
-        },
-        {
-          label: 'Basics',
-          items: [
-            // { label: '', link: '/' },
-          ]
-        },
+      fr: {
+        label: 'Français',
+        lang: 'fr'
+      }
+    },
+    social: {
+      github: 'https://github.com/AtRiskMedia',
+      twitter: 'https://twitter.com/AtRiskMedia',
+      discord: 'https://twitter.com/AtRiskMedia'
+    },
+    customCss: ['@fontsource/outfit', './src/styles/custom.css'],
+    sidebar: [{
+      label: 'Start here!',
+      items: [{
+        label: 'Getting Started',
+        link: '/start-here/getting-started',
+        badge: {
+          text: 'Important!',
+          variant: 'success'
+        }
+      }
+      // { label: '', link: '/' },
       ]
-    }),
-  ]
+    }, {
+      label: 'Core Concepts',
+      items: [
+        // { label: '', link: '/' },
+      ]
+    }, {
+      label: 'Tutorials',
+      items: [
+        // { label: '', link: '/' },
+      ]
+    }, {
+      label: 'Basics',
+      items: [
+        // { label: '', link: '/' },
+      ]
+    }]
+  }), tailwind()]
 });
