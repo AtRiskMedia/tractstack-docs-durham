75d4d7836eb82cc41592391bb239f07e24996c50
unknown <andrewgrahamhshs@gmail.com>
2024-04-19 15:11:20 -0400

    [Create] tab-component.mdx - Created the tab component internal reference page - Added the page to astro.config.mjs to allow for the website to recognize it as something visitable

--- astro.config.mjs
+++ astro.config.mjs
@@ -58,6 +58,7 @@ export default defineConfig({
       label: 'Internal Reference',
       items: [
         { label: 'Content Highlighting', link: '/internal-reference/content-highlighting' },
+        { label: 'Tab Component', link: '/internal-reference/tab-component' },
       ]
     },
   ]
