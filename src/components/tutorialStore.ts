3dfeeed427e80d833c5efe56e88ca161b5e8342e
Josh Frederick <1joshfrederick@gmail.com>
2024-04-30 16:55:24 -0400

    [Update] tutorialStore.ts & CheckboxComponent.astro
    -Changed checkMap to be a persistentMap

--- src/components/tutorialStore.ts
+++ src/components/tutorialStore.ts
@@ -1,10 +1,14 @@
 import { atom, map } from 'nanostores';
+import { persistentMap } from '@nanostores/persistent'
 
 export interface CheckMap {
     [key:string|number]:{
-        [key:string|number]:{checked:boolean; title:string; tutorial:string;};
+        [key:string|number]:{checked:boolean; itemTitle:string; tutorial:string;};
     };
 }
 export const isChecked = atom(false);
 
-export const checkMap = map<CheckMap>();
+export const checkMap = persistentMap<CheckMap>('checkmap:', {},{
+  encode: JSON.stringify,
+  decode: JSON.parse,
+})
