ed571dd74ecaad61b0a7ea04f29db42e6319020f
Josh Frederick <1joshfrederick@gmail.com>
2024-04-30 10:49:39 -0400

    [UPDATE] Tutorial Tracker
    -Updated CheckboxComponent.astro and tutorialStore.ts to be more in line with new code from yesterday in updated file structure

--- src/components/tutorialStore.ts
+++ src/components/tutorialStore.ts
@@ -2,8 +2,9 @@ import { atom, map } from 'nanostores';
 
 export interface CheckMap {
     [key:string|number]:{
-        [key:string|number]:boolean;
+        [key:string|number]:{checked:boolean};
     };
 }
+export const isChecked = atom(false);
 
 export const checkMap = map<CheckMap>();
