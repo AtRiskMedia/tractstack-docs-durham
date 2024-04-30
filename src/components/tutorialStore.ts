405093e86f9f109bdf0be3cd3246014d24376f7b
unknown <andrewgrahamhshs@gmail.com>
2024-04-30 10:40:27 -0400

    [Update] CheckboxComponent.astro tutorialStore.ts  - Implemented multidimensional array aspect walked through via call on April 29 2024

--- src/components/tutorialStore.ts
+++ src/components/tutorialStore.ts
@@ -2,7 +2,7 @@ import { atom, map } from 'nanostores';
 
 export interface CheckMap {
     [key:string|number]:{
-        [key:string|number]:boolean;
+        [key:string|number]:{checked:boolean};
     };
 }
 export const isChecked = atom(false);
